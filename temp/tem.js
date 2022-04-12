ad179fafffeda01367bc07e929d33d5133d0f444
Haiyan-Productions <102472286+Haiyan-Productions@users.noreply.github.com>
2022-04-12 06:38:13 +0500

    Add files via upload

---
+++ temp/tem.js
@@ -0,0 +1,14 @@
+// for ramdamm modale
+
+
+
+
+window.onload = function () {
+    var delayInMilliseconds = 2500; //1 second
+
+    setTimeout(function () {
+        //your code to be executed after 1 second
+        document.getElementById("ramdanbtn").click();
+    }, delayInMilliseconds);
+
+}
